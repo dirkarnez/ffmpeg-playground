abc40a0f2465bedfc1a1826ad84895e21540e53e
dirkarnez <smalldirkalex@gmail.com>
2022-08-18 23:37:25 +0800

    - init, working

---
+++ README.md
@@ -0,0 +1,2 @@
+ffmpeg-clang-playground
+=======================
---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+extern	"C" {
+#include "libavcodec/avcodec.h"
+#include "libavformat/avformat.h"
+#include "libavdevice/avdevice.h"
+#include "libavutil/avutil.h"
+#include "libavfilter/avfilter.h"
+}
+
+using namespace std;
+
+int main() {
+    avdevice_register_all();
+    AVFormatContext *pFormatCtx = avformat_alloc_context();
+	AVDictionary *options = NULL;
+	av_dict_set(&options, "list_devices", "true", 0);
+	const AVInputFormat *iformat = av_find_input_format("dshow");
+	cout << "=============Device Info================="<< endl;
+	avformat_open_input(&pFormatCtx, "video=dummy", iformat, &options);
+    cout << "========================================="<< endl;
+    cin.get();
+    return 0;
+}
